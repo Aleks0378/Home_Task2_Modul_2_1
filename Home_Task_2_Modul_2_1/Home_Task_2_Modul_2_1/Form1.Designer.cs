81d21288360bdbddfb930220fb95b1e98345c9fd
Aleks0378 <128078377+Aleks0378@users.noreply.github.com>
2023-04-29 02:09:51 +0300

    Add files via upload

---
+++ Home_Task_2_Modul_2_1/Home_Task_2_Modul_2_1/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Home_Task_2_Modul_2_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.surname = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.second_name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.add_info = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.save = new System.Windows.Forms.Button();
+            this.load = new System.Windows.Forms.Button();
+            this.dateTimePickerBirthday = new System.Windows.Forms.DateTimePicker();
+            this.gender = new System.Windows.Forms.ComboBox();
+            this.marital_status = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Surname";
+            // 
+            // surname
+            // 
+            this.surname.Location = new System.Drawing.Point(193, 27);
+            this.surname.Name = "surname";
+            this.surname.Size = new System.Drawing.Size(323, 22);
+            this.surname.TabIndex = 1;
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(193, 74);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(323, 22);
+            this.name.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // second_name
+            // 
+            this.second_name.Location = new System.Drawing.Point(193, 119);
+            this.second_name.Name = "second_name";
+            this.second_name.Size = new System.Drawing.Size(323, 22);
+            this.second_name.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Second name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(57, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Date of birthday";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(57, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Marital status";
+            // 
+            // add_info
+            // 
+            this.add_info.Location = new System.Drawing.Point(193, 324);
+            this.add_info.Multiline = true;
+            this.add_info.Name = "add_info";
+            this.add_info.Size = new System.Drawing.Size(441, 97);
+            this.add_info.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(57, 324);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Additional info.";
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(590, 27);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 14;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // load
+            // 
+            this.load.Location = new System.Drawing.Point(590, 67);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(75, 23);
+            this.load.TabIndex = 15;
+            this.load.Text = "Load";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // dateTimePickerBirthday
+            // 
+            this.dateTimePickerBirthday.Location = new System.Drawing.Point(193, 220);
+            this.dateTimePickerBirthday.MaxDate = new System.DateTime(2023, 4, 27, 0, 0, 0, 0);
+            this.dateTimePickerBirthday.Name = "dateTimePickerBirthday";
+            this.dateTimePickerBirthday.Size = new System.Drawing.Size(264, 22);
+            this.dateTimePickerBirthday.TabIndex = 16;
+            this.dateTimePickerBirthday.Value = new System.DateTime(2023, 4, 27, 0, 0, 0, 0);
+            // 
+            // gender
+            // 
+            this.gender.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.gender.FormattingEnabled = true;
+            this.gender.Location = new System.Drawing.Point(193, 168);
+            this.gender.Name = "gender";
+            this.gender.Size = new System.Drawing.Size(137, 24);
+            this.gender.TabIndex = 17;
+            // 
+            // marital_status
+            // 
+            this.marital_status.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.marital_status.FormattingEnabled = true;
+            this.marital_status.Location = new System.Drawing.Point(193, 269);
+            this.marital_status.Name = "marital_status";
+            this.marital_status.Size = new System.Drawing.Size(137, 24);
+            this.marital_status.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 433);
+            this.Controls.Add(this.marital_status);
+            this.Controls.Add(this.gender);
+            this.Controls.Add(this.dateTimePickerBirthday);
+            this.Controls.Add(this.load);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.add_info);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.second_name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.surname);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(720, 480);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.WhiteSmoke;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox surname;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox second_name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox add_info;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBirthday;
+        private System.Windows.Forms.ComboBox gender;
+        private System.Windows.Forms.ComboBox marital_status;
+    }
+}
+
---
+++ Home_Task_2_Modul_2_1/Home_Task_2_Modul_2_1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Home_Task_2_Modul_2_1
+{
+    public partial class Form1 : Form
+    {   
+        public Form1()
+        {
+            InitializeComponent();
+            gender.Items.Add("male");
+            gender.Items.Add("female");
+            marital_status.Items.Add("marriage");
+            marital_status.Items.Add("single");
+        }
+
+        public void save_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>()
+            { surname.Text, name.Text, second_name.Text, gender.Text, dateTimePickerBirthday.Value.ToLongDateString() , marital_status.Text, add_info.Text };
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<string>));
+            try
+            {
+                using (Stream fStream = File.Create("Form1.xml"))
+                {
+                    xmlFormat.Serialize(fStream, list);
+                }
+                MessageBox.Show("XmlSerialize OK!", "Message");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void load_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+            //Form1 form = new Form1();
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<string>));
+            try
+            {
+                using (Stream fStream = File.OpenRead("Form1.xml"))
+                {
+                    list = (List<string>)xmlFormat.Deserialize(fStream);
+                }
+                this.surname.Text = list[0];
+                this.name.Text = list[1];
+                this.second_name.Text = list[2];
+                this.gender.Text = list[3];
+                this.dateTimePickerBirthday.Text = list[4];
+                this.marital_status.Text = list[5];
+                this.add_info.Text = list[6];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
